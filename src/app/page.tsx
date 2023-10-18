a13be986faad218bca52228d403b490fafdade68
Kevin Almeida <kevin.almeidafsa@gmail.com>
2023-10-17 23:34:46 -0300

    chore: add shadcn setup lucide-react

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <>
-      <div>
-        <Image
-          src="/images/nextjs.png"
-          alt="Next.js Logo"
-          width={200}
-          height={160}
-        />
-        <h1>Next.js</h1>
-        <p>A React Framework for Production</p>
-      </div>
-    </>
-  );
+  return <div></div>;
 }
