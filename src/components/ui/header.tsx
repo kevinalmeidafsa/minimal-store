eab534e4c99a9cb2fa4ed57fb566dc823663a0f8
Kevin Almeida <kevin.almeidafsa@gmail.com>
2023-10-31 21:47:17 -0300

    feat: add seed script to prisma and imageUrls field to Product Table

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -53,8 +53,8 @@ const Header = () => {
             <SheetHeader className="text-left text-lg font-semibold">
               Menu
             </SheetHeader>
-            {status === "authenticated" && data?.user && (
-              <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-4">
+              {status === "authenticated" && data?.user && (
                 <div className="flex items-center gap-3">
                   <Avatar>
                     <AvatarFallback>
@@ -69,19 +69,6 @@ const Header = () => {
                     <p className="text-xs opacity-60">{data.user.email}</p>
                   </div>
                 </div>
-                <Separator className="m-auto w-3/4" />
-              </div>
-            )}
-
-            <div className=" mt-4 flex h-full w-full flex-col gap-3">
-              {status === "unauthenticated" && (
-                <Button
-                  variant={"outline"}
-                  onClick={handleLoginClick}
-                  className="flex w-full items-center justify-start gap-3 rounded-lg"
-                >
-                  <User /> Fazer Login
-                </Button>
               )}
               {status === "authenticated" && (
                 <Button
@@ -92,6 +79,19 @@ const Header = () => {
                   <LogOutIcon /> Logout
                 </Button>
               )}
+              {status === "unauthenticated" && (
+                <Button
+                  variant={"outline"}
+                  onClick={handleLoginClick}
+                  className="flex w-full items-center justify-start gap-3 rounded-lg"
+                >
+                  <User /> Fazer Login
+                </Button>
+              )}
+              <Separator className="m-auto w-3/4" />
+            </div>
+
+            <div className=" mt-4 flex h-full w-full flex-col gap-3">
               <Button
                 variant={"outline"}
                 className="flex w-full items-center justify-start gap-3 rounded-lg"
@@ -127,7 +127,9 @@ const Header = () => {
             </Button>
           </SheetTrigger>
           <SheetContent side={"right"}>
-            <SheetHeader>Carrinho</SheetHeader>
+            <SheetHeader className="text-left text-lg font-semibold">
+              Carrinho
+            </SheetHeader>
           </SheetContent>
         </Sheet>
       </Card>
